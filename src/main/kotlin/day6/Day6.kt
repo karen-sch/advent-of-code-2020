c9dd3adb25874c8330fc8983a68ef76e73a7f587
Karen Schwane <karen.schwane@schibsted.com>
2020-12-06 12:27:29 +0100

    Function references everywhere

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -23,7 +23,8 @@ fun parseGroupsGetAnswersByAnyone(fileContent: String): Int {
 
 fun parseGroupsGetAnswersByEveryone(fileContent: String): Int {
     return fileContent.split(DOUBLE_NEW_LINE).map { group ->
-        val people = group.lines().filter { it.isNotBlank() }.map(String::toSet)
+        val people = group.lines().filter(String::isNotBlank).map(String::toSet)
+
         group.toCharArray().distinct().filterNot(Char::isWhitespace).filter { char ->
             people.all { person -> person.contains(char) }
         }
