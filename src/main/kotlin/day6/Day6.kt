b48e03248dd8abb1e28a8996b7964fdb66c6b6a9
Karen Schwane <karen.schwane@schibsted.com>
2020-12-06 11:30:54 +0100

    Make nicer?

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -3,8 +3,6 @@ package day6
 import common.fileFromResources
 
 val DOUBLE_NEW_LINE = Regex("\n\n")
-val NEW_LINE = Regex("\n")
-val WHITESPACE = Regex("\\s")
 
 fun main() {
     Day6.input?.let {
@@ -19,22 +17,17 @@ fun main() {
 
 fun parseGroupsGetAnswersByAnyone(fileContent: String): Int {
     return fileContent.split(DOUBLE_NEW_LINE).map {
-        it.replace(WHITESPACE, "").toCharArray().toSet()
+        it.toCharArray().distinct().filterNot(Char::isWhitespace)
     }.sumBy { it.size }
 }
 
 fun parseGroupsGetAnswersByEveryone(fileContent: String): Int {
     return fileContent.split(DOUBLE_NEW_LINE).map { group ->
-        val charToCount = HashMap<Char, Int>()
-        val people = group.split(NEW_LINE).filter { it.isNotBlank() }
-        people.forEach { answerForPerson ->
-            answerForPerson.forEach { char ->
-                charToCount[char] = charToCount[char]?.plus(1) ?: 1
-            }
+        val people = group.lines().filter { it.isNotBlank() }
+        group.toCharArray().distinct().filterNot(Char::isWhitespace).filter { char ->
+            people.all { person -> person.contains(char) }
         }
-
-        charToCount.values.count { it == people.size }
-    }.sum()
+    }.sumBy { it.size }
 }
 
 object Day6 {
