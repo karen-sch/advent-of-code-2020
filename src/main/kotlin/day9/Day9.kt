7208fbda199fbae181a8984cf7d52beaba36fa88
Karen Schwane <karen.schwane@schibsted.com>
2020-12-09 11:06:20 +0100

    Nicer?

--- src/main/kotlin/day9/Day9.kt
+++ src/main/kotlin/day9/Day9.kt
@@ -1,6 +1,7 @@
 package day9
 
 import common.fileFromResources
+import java.lang.IllegalArgumentException
 
 fun main() {
     Day9.input?.let {
@@ -15,44 +16,29 @@ fun main() {
 fun findFirstNonSumNumber(numbers: List<Long>, preambleLength: Int = 25): Long {
     (preambleLength until numbers.size).forEach { i ->
         val number = numbers[i]
+        val possibleSummands = (i - preambleLength until i).map { numbers[it] }.toSet()
 
-        val possibleSummands = (i-preambleLength until i).map { numbers[it] }
-        var found = false
-
-        val seen = HashSet<Long>()
-        possibleSummands.forEach findSum@{ summand ->
-
-            if (seen.contains(number - summand)) {
-                found = true
-                return@findSum
-            }
-            seen.add(summand)
-
-        }
-
-        if (!found) return number
+        if (possibleSummands.none() {
+                possibleSummands.minus(it).contains(number - it)
+            }) return number
     }
 
-    return -1
+    throw IllegalArgumentException("No non-sum number found for input data")
 }
 
 fun findContiguousSetThatSumUpTo(sum: Long, numbers: List<Long>): Long {
-    numbers.forEachIndexed { index, summand1 ->
-        var currentSum = summand1
-        var index2 = index + 1
-        val summands = mutableListOf<Long>()
-        summands.add(summand1)
-        while (currentSum < sum && index2 < numbers.size) {
-            currentSum += numbers[index2]
-            summands.add(numbers[index2])
-            index2++
+    numbers.forEachIndexed { i, number ->
+        var j = i + 1
+        val summands = mutableListOf(number)
+        while (summands.sum() < sum && j < numbers.size) {
+            summands.add(numbers[j])
+            j++
         }
-        if (currentSum == sum) {
+        if (summands.sum() == sum) {
             return summands.minOrNull()!! + summands.maxOrNull()!!
         }
     }
-
-    return -1
+    throw IllegalArgumentException("No contiguous set that sums up to $sum found in input data")
 }
 
 object Day9 {
