9f34a0e16569cf3667b2e99a623c45cff90018d0
Karen Schwane <karen.schwane@schibsted.com>
2020-12-10 10:44:49 +0100

    Remember previously seen permutations

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -6,6 +6,9 @@ fun main() {
     Day10.input?.let {
         val result1 = get1JoltDiffTimes3JoltDiffs(it)
         println("Part 1: $result1")
+
+        val result2 = countDifferentWays(it)
+        println("Part 2: $result2")
     }
 }
 
@@ -16,14 +19,11 @@ fun get1JoltDiffTimes3JoltDiffs(input: List<Int>): Int {
     val sorted = input.sorted()
     val jolts = listOf(0) + sorted + (sorted.last() + 3)
 
-
     (1..jolts.lastIndex).forEach { i ->
-
         when (jolts[i] - jolts[i - 1]) {
             1 -> diff1++
             3 -> diff3++
         }
-
     }
 
     return diff1 * diff3
@@ -33,28 +33,34 @@ fun countDifferentWays(input: List<Int>): Long {
     val sorted = input.sorted()
     val jolts = listOf(0) + sorted + (sorted.last() + 3)
 
-    return generatePermutations(jolts, 0)
+    return generatePermutations(jolts, mutableMapOf(), 0)
 }
 
-fun generatePermutations(jolts: List<Int>,  permutations: Long): Long {
+fun generatePermutations(jolts: List<Int>,
+                         seenPermutations: MutableMap<Int, Long>,
+                         sum: Long): Long {
     if (jolts.size == 1) {
-        return permutations + 1
+        return sum + 1
     }
 
-    var sum = 0L
-    if (jolts.size > 1 && jolts[1] - jolts.first() in 1..3) {
-        sum += generatePermutations(jolts.drop(1), permutations)
-    }
+    return countPermutationsForIndex(jolts, 1, seenPermutations, sum) +
+            countPermutationsForIndex(jolts, 2, seenPermutations, sum) +
+            countPermutationsForIndex(jolts, 3, seenPermutations, sum)
 
-    if (jolts.size > 2 && jolts[2] - jolts.first() in 1..3) {
-        sum += generatePermutations(jolts.drop(2), permutations)
-    }
+}
 
-    if (jolts.size > 3 && jolts[3] - jolts.first() in 1..3) {
-        sum += generatePermutations(jolts.drop(3), permutations)
+private fun countPermutationsForIndex(
+    jolts: List<Int>,
+    index: Int,
+    seenPermutations: MutableMap<Int, Long>,
+    sum: Long
+): Long {
+    if (jolts.size > index && jolts[index] - jolts.first() in 1..3) {
+        return seenPermutations.computeIfAbsent(jolts.size - index) {
+            generatePermutations(jolts.drop(index), seenPermutations, sum)
+        }
     }
-
-    return sum
+    return 0
 }
 
 object Day10 {
