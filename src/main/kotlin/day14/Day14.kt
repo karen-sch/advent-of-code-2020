073905bbb5d765866efbf763a48649213e6609bf
Karen Schwane <karen.schwane@schibsted.com>
2020-12-14 20:37:27 +0100

    Clean up a little

--- src/main/kotlin/day14/Day14.kt
+++ src/main/kotlin/day14/Day14.kt
@@ -4,29 +4,25 @@ package day14
 
 import common.fileFromResources
 
-/*
-mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-mem[8] = 11
-mem[7] = 101
-mem[8] = 0
- */
 val MASK_REGEX = Regex("([10X]+)")
 val MEM_MASK = Regex("mem\\[(\\d+)\\] = (\\d+)")
 
 @ExperimentalUnsignedTypes
 fun main() {
     Day14.input?.let {
-        val result1 = parseInputPart1(it)
+        val result1 = parseInput(it, ::applyMaskPart1)
         println("Part 1: $result1")
 
-        val result2 = parseInputPart2(it)
+        val result2 = parseInput(it, ::applyMaskPart2)
         println("Part 2: $result2")
     }
 }
 
-
 @ExperimentalUnsignedTypes
-fun parseInputPart1(lines: List<String>): ULong {
+fun parseInput(
+    lines: List<String>,
+    applyMask: (address: ULong, number: ULong, mask: String, memory: HashMap<ULong, ULong>) -> Unit
+): ULong {
     val memory = HashMap<ULong, ULong>()
     var mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
@@ -38,43 +34,18 @@ fun parseInputPart1(lines: List<String>): ULong {
         } else if (line.startsWith("mem")) {
             MEM_MASK.find(line)?.let {
                 val (address, value) = it.destructured
-                val maskedValue = applyMaskPart1(value.toULong(), mask)
-                memory[address.toULong()] = maskedValue
+                applyMask(address.toULong(), value.toULong(), mask, memory)
             }
         }
     }
 
     return memory.values.sum()
 }
-
-@ExperimentalUnsignedTypes
-fun parseInputPart2(lines: List<String>): ULong {
-    val memory = HashMap<ULong, ULong>()
-    var mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-
-    lines.forEach { line ->
-        if (line.startsWith("mask")) {
-            MASK_REGEX.find(line)?.groupValues?.get(1)?.let {
-                mask = it
-            }
-        } else if (line.startsWith("mem")) {
-            MEM_MASK.find(line)?.let {
-                val (address, value) = it.destructured
-                applyMaskPart2(address.toULong(), value.toULong(), mask, memory)
-            }
-        }
-    }
-
-    return memory.values.sum()
-}
-
 @ExperimentalUnsignedTypes
 fun applyMaskPart2(address: ULong, number: ULong, mask: String, memory: HashMap<ULong, ULong>) {
-    val one: ULong = 1u
-    //var solution: ULong = 0u
     val solutions = mutableListOf<ULong>(0u)
     (0..35).forEach { bitAt ->
-        val bit = (address shr bitAt) and one
+        val bit = (address shr bitAt) and 1u
         val maskAtIndex = mask[mask.lastIndex - bitAt]
         when (maskAtIndex) {
             '0' -> {
@@ -105,11 +76,10 @@ fun applyMaskPart2(address: ULong, number: ULong, mask: String, memory: HashMap<
 }
 
 @ExperimentalUnsignedTypes
-fun applyMaskPart1(number: ULong, mask: String): ULong {
-    val one: ULong = 1u
+fun applyMaskPart1(address: ULong, number: ULong, mask: String, memory: HashMap<ULong, ULong>) {
     var solution: ULong = 0u
     (0..35).forEach { bitAt ->
-        val bit = (number shr bitAt) and one
+        val bit = (number shr bitAt) and 1u
         val maskAtIndex = mask[mask.lastIndex - bitAt]
         solution = if (maskAtIndex != 'X') {
             val maskValue = Integer.parseInt(maskAtIndex.toString()).toULong()
@@ -118,7 +88,7 @@ fun applyMaskPart1(number: ULong, mask: String): ULong {
             solution or (bit shl bitAt)
         }
     }
-    return solution
+    memory[address] = solution
 }
 
 object Day14 {
