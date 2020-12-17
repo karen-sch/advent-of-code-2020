0bf5b3ea4bd35d3574e775e928389ef35c15ea26
Karen Schwane <karen.schwane@schibsted.com>
2020-12-17 18:55:18 +0100

    Day 17, part 1 works

--- src/main/kotlin/day17/Day17.kt
+++ src/main/kotlin/day17/Day17.kt
@@ -26,48 +26,63 @@ fun part1(lines: List<String>): Int {
             }
         }
     }
-    val map = parseInputPart1(lines)
+    var map = parseInputPart1(lines, directions)
     repeat(6) {
         println(countActiveCubes(map))
-        applyPatternPart1(map, directions)
+        map = applyPatternPart1(map, directions)
     }
     return countActiveCubes(map)
 }
 
-fun parseInputPart1(lines: List<String>): HashMap<Int, HashMap<Int, HashMap<Int, Char>>> {
+fun parseInputPart1(lines: List<String>, directions: List<List<Int>>): HashMap<Int, HashMap<Int, HashMap<Int, Char>>> {
     val map = HashMap<Int, HashMap<Int, HashMap<Int, Char>>>()
     lines.mapIndexed { y, line ->
         line.mapIndexed { x, char ->
-            val xMap = map.computeIfAbsent(x) {
+            val xMap = map.getOrPut(x) {
                 HashMap()
             }
-            val yMap = xMap.computeIfAbsent(y) {
+            val yMap = xMap.getOrPut(y) {
                 HashMap()
             }
             yMap[0] = char
+
+            directions.forEach { (xDelta, yDelta, zDelta) ->
+                val value = map[x + xDelta]?.get(y + yDelta)?.get(zDelta)
+                if (value == null) {
+                    map.getOrPut(x + xDelta) { HashMap() }
+                        .getOrPut(y + yDelta) { HashMap() }[zDelta] = INACTIVE
+                }
+            }
         }
     }
     return map
 }
 
-fun applyPatternPart1(map: HashMap<Int, HashMap<Int, HashMap<Int, Char>>>, directions: List<List<Int>>) {
+fun applyPatternPart1(map: HashMap<Int, HashMap<Int, HashMap<Int, Char>>>, directions: List<List<Int>>):
+        HashMap<Int, HashMap<Int, HashMap<Int, Char>>> {
+    val newMap = HashMap<Int, HashMap<Int, HashMap<Int, Char>>>()
     map.entries.forEach { (x, xMap) ->
         xMap.entries.forEach { (y, yMap) ->
             yMap.entries.forEach { (z, char) ->
                 val adjacent = directions.map { (xDelta, yDelta, zDelta) ->
-                    map.computeIfAbsent(x + xDelta) { HashMap() }
-                        .computeIfAbsent(y + yDelta) { HashMap() }
-                        .computeIfAbsent(z + zDelta) { INACTIVE }
+                    val value = map[x + xDelta]?.get(y + yDelta)?.get(z + zDelta)
+                    if (value == null && newMap[x + xDelta]?.get(y + yDelta)?.get(z + zDelta) == null) {
+                        newMap.getOrPut(x + xDelta) { HashMap() }
+                            .getOrPut(y + yDelta) { HashMap() }[z + zDelta] = INACTIVE
+                    }
+                    value ?: INACTIVE
                 }
                 val newValue = when (char) {
                     ACTIVE -> if (adjacent.count { it == ACTIVE } in (2..3)) ACTIVE else INACTIVE
                     INACTIVE -> if (adjacent.count { it == ACTIVE } == 3) ACTIVE else INACTIVE
                     else -> throw IllegalArgumentException("Illegal char $char")
                 }
-                yMap[z] = newValue
+                newMap.getOrPut(x) { HashMap() }.getOrPut(y) { HashMap() }[z] = newValue
             }
         }
     }
+
+    return newMap
 }
 
 fun countActiveCubes(map: HashMap<Int, HashMap<Int, HashMap<Int, Char>>>): Int {
